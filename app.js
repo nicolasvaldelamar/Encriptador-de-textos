f5365445e7acf3987a94f90558204a0c08bcbf00
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2022-12-22 17:45:49 -0500

    encriptado implementado

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const btn1 = document.querySelector('.css-button-retro--sky')
 const btn2 = document.querySelector('.css-button-retro--sky1')
 const no = document.querySelector('.no')
 const si = document.querySelector('.si')
-
-
-
+const output = document.querySelector('.output')
+const noencontrado = document.querySelector('.no-encontrado')
+const palabra = document.querySelector('.palabra')
 
 input?.addEventListener('change', Actualizar)
 btn1?.addEventListener('click', Encriptar)
 btn2?.addEventListener('click', Dencriptar)
-
+let cadena;
 let array;
 let r;
-
+let cadena2;
 function Actualizar(){
     array = Array.from((input.value).toLowerCase());
 }
@@ -38,24 +38,35 @@ function Encriptar(){
      }
    }
    r = array;
+   noencontrado.style.visibility = 'hidden';
+   palabra.innerText = ""
+   palabra.style.fontSize = "18pt";
    console.log(r)
-   return r
+   cadena2 = r[0]
+   for(let i = 0; i < r.length - 1; i++){
+
+    cadena2 += r[i+1]
+    console.log(cadena2)
+   }
+   palabra.innerText = cadena2
 }
 
 function Dencriptar(){
+    
+    cadena = (input.value).toLowerCase();
 
-    let cadena = (input.value).toLowerCase();
-
-    var posicione = cadena.indexOf("enter");
-    var posicioni = cadena.indexOf("imes");
-    var posiciona = cadena.indexOf("ai");
-    var posicionu = cadena.indexOf("ufat");
-    var posiciono = cadena.indexOf("ober");
+    let posicione = cadena.indexOf("enter");
+    let posicioni = cadena.indexOf("imes");
+    let posiciona = cadena.indexOf("ai");
+    let posicionu = cadena.indexOf("ufat");
+    let posiciono = cadena.indexOf("ober");
     let posiciones = [posicione,posicioni,posiciona,posicionu,posiciono];
     let desencriptar = ['enter','imes','ai','ufat','ober'];
     for(let i = 0; i < desencriptar.length; i++){
          whilee(posiciones[i], desencriptar[i]);
     }
+    document.input.style.visibility = 'hidden';
+    document.output.style.visibility = 'visible';
     
 }
     
@@ -63,7 +74,21 @@ function Dencriptar(){
 
 function whilee(posicion, palabra){
     while (posicion >= 0){
-        cadena = cadena.slice(0, posicion) + "atito" + cadena.slice(posicion + 3);
-        posicion = cadena.indexOf("ato");
+        if(palabra === "enter")
+        cadena = cadena.slice(0, posicion) + "e" + cadena.slice(posicion + 3);
+        posicion = cadena.indexOf(palabra);
+        if(palabra === "imes")
+        cadena = cadena.slice(0, posicion) + "i" + cadena.slice(posicion + 3);
+        posicion = cadena.indexOf(palabra);
+        if(palabra === "ai")
+        cadena = cadena.slice(0, posicion) + "a" + cadena.slice(posicion + 3);
+        posicion = cadena.indexOf(palabra);
+        if(palabra === "ufat")
+        cadena = cadena.slice(0, posicion) + "u" + cadena.slice(posicion + 3);
+        posicion = cadena.indexOf(palabra);
+        if(palabra === "ober")
+        cadena = cadena.slice(0, posicion) + "o" + cadena.slice(posicion + 3);
+        posicion = cadena.indexOf(palabra);
     }
+    
 }
